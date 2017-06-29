bef0f5d7f1309be74ea4c721410e3ac59e42f53d
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2017-06-29 16:50:45 +0300

    Introduce "use expression body" inspection #KT-16063 Fixed
    Converted from the relevant intention
    Reported cases: one-liners, whens
    Also, more exact caret detection in local inspection tests

--- idea/testData/quickfix/moveReceiverAnnotation/notExtensionFun.kt
+++ idea/testData/quickfix/moveReceiverAnnotation/notExtensionFun.kt
@@ -1,6 +1,5 @@
 // "Move annotation to receiver type" "false"
 // ERROR: '@receiver:' annotations could be applied only to extension function or extension property declarations
-// ACTION: Convert to expression body
 // ACTION: Make internal
 // ACTION: Make private
 
